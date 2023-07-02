ce1d57cbc6b2a286d7edfbefdfd32cf065decd5e
Vivek Kumar Gupta <98837236+svivekgupta@users.noreply.github.com>
2023-07-03 01:53:48 +0530

    Add files via upload

---
+++ vivek.js.js
@@ -0,0 +1,11 @@
+function search() {
+    // Get the input value
+    var searchQuery = document.getElementById("searchInput").value;
+    
+    // Perform the search (replace with your own logic)
+    console.log("Performing search for: " + searchQuery);
+    
+    // Clear the input field
+    document.getElementById("searchInput").value = "";
+  }
+  
